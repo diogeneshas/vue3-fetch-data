37009650d37778a458e121b6b7c3049bac848c42
diogeneshas <diogenes.henrique.br@gmail.com>
2023-05-11 07:34:07 -0300

    Making the HTTP Request with axios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+import BreakingBadCards from "./components/BreakingBadCards.vue"
 </script>
 
 <template>
+  <h1>Hero</h1>
+  <BreakingBadCards />
 </template>
 
 <style scoped>
---
+++ src/components/BreakingBadCards.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Breaking Bard Cards</h1>
+  </div>
+</template>
+
+<script setup>
+import axios from "axios"
+
+const response =  await axios.get("https://thronesapi.com/api/v2/Characters")
+
+console.log(response);
+</script>
