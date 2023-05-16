3ac13745cffa295449ca03e684ac6b51b13da309
diogeneshas <diogenes.henrique.br@gmail.com>
2023-05-16 18:46:02 -0300

    Building the UI

--- src/components/BreakingBadCards.vue
+++ src/components/BreakingBadCards.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-    <h1>Breaking Bard Cards</h1>
-    <p>{{ characters }}</p>
-    <div>
-      <button @click="page = page + 1">Next</button>
-      <button>Back</button>
+  <div class="container">
+    <div class="cards">
+      <Card v-for="character in characters" :key="character.id" :image="character.image" :name="character.name" :occupation="character.occupation"/>
     </div>
   </div>
 </template>
 
 <script setup>
+import Card from "@/components/Card.vue"
 import { ref, watch } from "vue"
 import axios from "axios"
 
@@ -26,3 +24,49 @@ watch(page, async () => {
   { immediate: true }
 )
 </script>
+
+<style scoped>
+
+/* Breaking Bad Styles */
+
+.container {
+  background-color: rgb(27, 26, 26);
+  padding: 30px
+}
+.cards {
+  max-width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  height: 900px
+}
+.cards h3 {
+  font-weight: bold;
+}
+.cards p {
+  font-size: 10px;
+}
+.jobs {
+  display: flex;
+  flex-wrap: wrap;
+}
+.button-container {
+  display: flex;
+  justify-content: center;
+  padding-top: 30px
+}
+.button-container button {
+  border: none;
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  margin: 0 5px;
+  cursor: pointer;
+}
+.spinner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <n-card :title="name">
+    <template #cover>
+      <img :src="image" alt="">
+    </template>
+    <h4>{{ occupation }}</h4>
+  </n-card>
+</template>
+
+<script setup>
+import { defineProps } from "vue"
+
+const { image, name, occupation } = defineProps([
+  'image', 'name', 'occupation'
+])
+</script>
+
+<style>
+/* Card Styles */
+
+.n-card {
+  width: 200px;
+  margin:10px 20px;
+
+}
+.n-card img {
+  height: 250px
+}
+
+p {
+  font-size: 10px;
+}
+
+.jobs {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
