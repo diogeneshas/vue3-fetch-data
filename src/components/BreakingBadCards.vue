7afdee2c9d9a3e7328b454a684ef2bd481efb6c3
diogeneshas <diogenes.henrique.br@gmail.com>
2023-05-16 10:50:55 -0300

    Implementing Pagination

--- src/components/BreakingBadCards.vue
+++ src/components/BreakingBadCards.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
     <h1>Breaking Bard Cards</h1>
+    <p>{{ characters }}</p>
+    <div>
+      <button @click="page = page + 1">Next</button>
+      <button>Back</button>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref, watch } from "vue"
 import axios from "axios"
 
-const response =  await axios.get("https://thronesapi.com/api/v2/Characters")
+const characters = ref(null)
+const page = ref(0)
 
+const response =  await axios.get("https://bobsburgers-api.herokuapp.com/characters?limit=8")
+characters.value = response.data
 
+watch(page, async () => {
+  const res =  await axios.get(`https://bobsburgers-api.herokuapp.com/characters/?limit=8&skip=${page.value * 8}`)
+  characters.value = res.data
+},
+  { immediate: true }
+)
 </script>
